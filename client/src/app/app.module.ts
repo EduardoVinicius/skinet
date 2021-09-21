708a4917a2969a9569a5bf9be1c3f3705a2e239f
Edvin <eduardo.vinicius2009@hotmail.com>
2021-09-21 09:14:00 -0300

    Add routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,7 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home/home.module';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { ShopModule } from './shop/shop.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
