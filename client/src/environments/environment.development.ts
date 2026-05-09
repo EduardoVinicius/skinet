15e94f4d0e37cd66048ede7e6a27fd648b0d240c
Eduardo Correia <eduardo.vinicius2009@hotmail.com>
2026-05-09 12:51:20 -0300

    End of section 16

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51TRVGVGzjqsb5wxmJlYx0tpeJ1aRcfZSHKeY0XS1wXNuNlfpxkG8rtPGFhMMjIwCwiibQJJicBMI7NaPVDNkuMOY00VVmIiezE'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51TRVGVGzjqsb5wxmJlYx0tpeJ1aRcfZSHKeY0XS1wXNuNlfpxkG8rtPGFhMMjIwCwiibQJJicBMI7NaPVDNkuMOY00VVmIiezE'
 };
